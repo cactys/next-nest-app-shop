7a1f8166371a2bf195967ed6ee6da6918a8e6e89
cactys <cactys95@yandex.ru>
2023-12-06 17:49:07 +0300

    add authenticated

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,13 +1,18 @@
 import {
   Body,
+  Request,
   Controller,
   Header,
   HttpCode,
   HttpStatus,
   Post,
+  UseGuards,
+  Get,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LocalAuthGuard } from 'src/auth/local.auth.guard';
+import { AuthenticatedGuard } from 'src/auth/authenticated.guard';
 
 @Controller('users')
 export class UsersController {
@@ -18,5 +23,25 @@ export class UsersController {
   @Header('Content-type', 'application/json')
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Post('/login')
+  @UseGuards(LocalAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Header('Content-type', 'application/json')
+  login(@Request() req) {
+    return { user: req.user, message: 'Logged in' };
+  }
+
+  @Get('/login-check')
+  @UseGuards(AuthenticatedGuard)
+  loginCheck(@Request() req) {
+    return req.user;
+  }
+
+  @Get('/logout')
+  logout(@Request() req) {
+    req.session.destroy();
+    return { message: 'Session has ended' };
   }
 }
