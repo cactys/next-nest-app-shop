845c42845bb5d7c1cade300d6de0213e27c083fd
cactys <cactys95@yandex.ru>
2024-04-24 17:35:49 +0300

    fis fix

--- client/components/modules/CatalogPage/FilterSelect.tsx
+++ client/components/modules/CatalogPage/FilterSelect.tsx
@@ -1,1 +1,47 @@
-https://youtu.be/qK1ENlEucpc?t=27853
+import { $mode } from '@/context/mode';
+import Select from 'react-select';
+import { TSelectOptionType } from '@/types/common';
+import { useUnit } from 'effector-react';
+import { useState } from 'react';
+import { createSelectOption } from '@/utils/common';
+
+const FilterSelect = () => {
+  const mode = useUnit($mode);
+  const [categoryOption, setCategoryOption] = useState<TSelectOptionType>(null);
+
+  const handleSearchOptionChange = (selectedOption: TSelectOptionType) => {
+    setCategoryOption(selectedOption);
+  };
+  https://youtu.be/qK1ENlEucpc?t=31509
+  return (
+    <Select
+      placeholder="Я ищу..."
+      value={categoryOption || createSelectOption('Сначала дешевле')}
+      onChange={handleSearchOptionChange}
+      styles={{
+        ...inputStyles,
+        control: (defaultStyles) => ({
+          ...controlStyles(defaultStyles, mode),
+        }),
+        input: (defaultStyles) => ({
+          ...defaultStyles,
+          color: mode === 'dark' ? '#f2f2f2' : '#222',
+        }),
+        menu: (defaultStyles) => ({
+          ...menuStyles(defaultStyles, mode),
+        }),
+        option: (defaultStyles, state) => ({
+          ...optionStyles(defaultStyles, state, mode),
+        }),
+      }}
+      isClearable={true}
+      openMenuOnClick={false}
+      options={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((item) => ({
+        value: item,
+        label: item,
+      }))}
+    />
+  );
+};
+
+export default FilterSelect;
---
+++ client/styles/catalog/select.ts
@@ -0,0 +1,141 @@
+import { IOption } from '@/types/common';
+import {
+  CSSObjectWithLabel,
+  GroupBase,
+  OptionProps,
+  StylesConfig,
+} from 'react-select';
+
+export const controlStyles = (
+  defaultStyles: CSSObjectWithLabel,
+  theme: string
+) => ({
+  ...defaultStyles,
+  cursor: 'pointer',
+  backgroundColor: 'transparent',
+  border: '1px solid #d5d5d5',
+  width: '241px',
+  height: '40px',
+  boxShadow: 'none',
+  borderRadius: '4px',
+  '&:hover': {
+    borderColor: '#9e9e9e',
+  },
+  '& .css-1dimb5e-singleValue': {
+    color: theme === 'dark' ? '#f2f2f2' : '#222',
+  },
+  '@media (max-width: 820px': {
+    width: '200px',
+  },
+  '@media (max-width: 560px)': {
+    width: '177px',
+  },
+});
+
+export const menuStyles = (
+  defaultStyles: CSSObjectWithLabel,
+  theme: string
+) => ({
+  ...defaultStyles,
+  boxShadow: '0 4px 20px rgb(0 0 0 / 7%)',
+  borderRadius: '4px',
+  height: 'auto',
+  overflow: 'hidden',
+  backgroundColor: theme === 'dark' ? '#2d2d2d' : '#f2f2f2',
+  width: 'calc(100% + 40px)',
+  minHeight: 30,
+});
+
+export const optionStyles = (
+  defaultStyles: CSSObjectWithLabel,
+  state: OptionProps<IOption, boolean, GroupBase<IOption>>,
+  theme: string
+) => {
+  const backgroundHoverFromLightMode = state.isSelected
+    ? state.isSelected
+      ? '#9e9e9e'
+      : '#f2f2f2'
+    : state.isSelected
+      ? '#f2f2f2'
+      : '#9e9e9e';
+
+  const backgroundHoverFromDarkMode = state.isSelected
+    ? state.isSelected
+      ? '#f2f2f2'
+      : '#9e9e9e'
+    : state.isSelected
+      ? '#9e9e9e'
+      : '#f2f2f2';
+
+  const colorHoverFromLightMode = state.isSelected
+    ? state.isSelected
+      ? '#f2f2f2'
+      : '#9e9e9e'
+    : state.isSelected
+      ? '#9e9e9e'
+      : '#f2f2f2';
+
+  const colorHoverFromDarkMode = state.isSelected
+    ? state.isSelected
+      ? '#9e9e9e'
+      : '#f2f2f2'
+    : state.isSelected
+      ? '#f2f2f2'
+      : '#9e9e9e';
+
+  return {
+    ...defaultStyles,
+    cursor: 'pointer',
+    padding: '6px 12px',
+    margin: 0,
+    '&:hover': {
+      backgroundColor:
+        theme === 'dark'
+          ? backgroundHoverFromDarkMode
+          : backgroundHoverFromLightMode,
+      color:
+        theme === 'dark' ? colorHoverFromDarkMode : colorHoverFromLightMode,
+    },
+    backgroundColor:
+      theme === 'dark'
+        ? state.isSelected
+          ? '#f2f2f2'
+          : '#2d2d2d'
+        : state.isSelected
+          ? '#2d2d2d'
+          : '#f2f2f2',
+    color:
+      theme === 'dark'
+        ? state.isSelected
+          ? '#222'
+          : '#f2f2f2'
+        : state.isSelected
+          ? '#f2f2f2'
+          : '#222',
+  };
+};
+
+export const inputStyles: StylesConfig<IOption, boolean, GroupBase<IOption>> = {
+  indicatorSeparator: () => ({ border: 'none' }),
+  dropdownIndicator: () => ({ display: 'none' }),
+  menuList: (defaultStyles) => ({
+    ...defaultStyles,
+    paddingTop: 0,
+    paddingBottom: 0,
+    minHeight: 30,
+    '&::-webkit-scrollbar': {
+      width: '8px',
+    },
+    '&::-webkit-scrollbar-track': {
+      background: 'transparent',
+    },
+    '&::-webkit-scrollbar-thumb': {
+      background: '#454545',
+      borderRadius: '3px',
+    },
+    '&::-webkit-scrollbar-thumb:hover': {
+      background: 'gray',
+    },
+  }),
+  placeholder: (defaultStyles) => ({ ...defaultStyles, color: '#b9babb' }),
+};
